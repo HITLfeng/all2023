5706d09dda6799119b7d19947392ebc62406c46a
fxj <123456@123.com>
2024-08-29 23:16:55 +0800

    feat:test frame
    202408292317

---
+++ kv-database/test/build.sh
@@ -0,0 +1,16 @@
+#！/bin/bash
+source /opt/rh/devtoolset-8/enable
+
+case "$1" in
+    clean)
+        # 清空目录
+        echo "clearing..."
+        rm main 2>/dev/null
+        echo "clearing done!"
+        ;;
+    *)
+        # 执行其他操作
+        source ./innerbuild.sh
+        ;;
+esac
+
